8ee495e00c54cf6a35f1656f3430fc8df58acc9e
Filip <filipdopa@yahoo.com>
2022-05-06 22:17:15 +0200

    Set routing

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 import fs from 'node:fs';
 import http from 'node:http';
+import path from 'node:path';
 
 const port = 3000;
 const ip = '127.0.0.1';
 
-function handleResponse(response) {
-  const readStream = fs.createReadStream('./public/index.html');
-  readStream.pipe(response);
+function handleResponse(request, response) {
+  const fileExtension = path.extname(request.url.toString());
+  console.log(request.url);
+  console.log(fileExtension);
+  const type = {
+    '.css': 'text/css',
+    '.git': 'image/gif',
+    '.htm': 'text/html',
+    '.html': 'text/html',
+    '.jpeg': 'image/jpeg',
+    '.jpg': 'image/jpg',
+    '.png': 'image/png',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon',
+    '.txt': 'text/plain',
+  };
+  const contentType = { 'Content-Type': type[fileExtension] };
+  let filePath = `./public${request.url}`;
+
+  // setting the default page
+  if (request.url === '/') {
+    contentType['Content-Type'] = 'text/html';
+    filePath = `./public/index.html`;
+  }
+
+  response.writeHead(200, contentType); // "Data description"
+  const readStream = fs.createReadStream(filePath); // Reading the requested file
+  readStream.pipe(response); // Forwarding the data to the user.
 }
 
+// Creating server
 const server = http.createServer((req, res) => {
-  console.log('Response url', res.url);
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  handleResponse(res);
+  handleResponse(req, res);
 });
 
 server.listen(port, ip, (err) => {
